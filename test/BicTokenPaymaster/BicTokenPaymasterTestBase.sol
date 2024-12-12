f36cadb010a23f26ea3cedee43d6b0d7dbe32f32
Broken moon <quyen.luminous@gmail.com>
2024-12-12 22:26:02 +0700

    (wip) setup testing smart account: entrypoint

--- test/BicTokenPaymaster/BicTokenPaymasterTestBase.sol
+++ test/BicTokenPaymaster/BicTokenPaymasterTestBase.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
  pragma solidity ^0.8.23;
 
+import "../../EntryPointv0.6/src/core/EntryPoint.sol";
 import "../../src/BicTokenPaymaster.sol";
 import "forge-std/Test.sol";
 import {Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
@@ -15,14 +16,18 @@ contract BicTokenPaymasterTestBase is Test {
     address public holder1 = vm.addr(holder1_pkey);
     uint256 public holder1_init_amount = 10000 * 1e18;
     address[] signers = [owner, dev];
+
+    EntryPoint entryPoint;
     function setUp() public virtual {
+        entryPoint = new EntryPoint();
+
         console.log("owner: ", owner);
         vm.prank(dev);
         address proxy = Upgrades.deployUUPSProxy(
             "BicTokenPaymaster.sol",
             abi.encodeCall(
                 BicTokenPaymaster.initialize,
-                (0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789, owner, signers)
+                (address(entryPoint), owner, signers)
             )
         );
         bic = BicTokenPaymaster(proxy);
