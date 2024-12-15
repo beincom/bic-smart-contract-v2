6bc744cd52b53cd6314fc9b30577f285aad6ee51
Broken moon <quyen.luminous@gmail.com>
2024-12-16 03:12:18 +0700

    (!) fix test Paymaster.t.sol

--- test/BicTokenPaymaster/smart-account/Paymaster.t.sol
+++ test/BicTokenPaymaster/smart-account/Paymaster.t.sol
@@ -6,17 +6,26 @@ import {BicTokenPaymasterTestBase} from "../BicTokenPaymasterTestBase.sol";
 
 contract TestPaymaster is BicTokenPaymasterTestBase {
     SimpleAccountFactory smart_account_factory;
+    uint256 user1_pkey = 0x001;
+    address user1 = vm.addr(user1_pkey);
+    uint256 public user1_init_amount = 10000 * 1e18;
+
+    address public randomExecutor = address(0xeee);
+
     function setUp() public virtual override {
         super.setUp();
         smart_account_factory = new SimpleAccountFactory(entrypoint);
-        bic.deposit({value: 1*1e18});
+        bic.deposit{value: 1*1e18}();
+        vm.prank(owner);
+        bic.transfer(user1, user1_init_amount);
     }
 
     function _setupUserOp(
         uint256 _signerPKey,
         bytes memory _initCode,
         bytes memory _callDataForEntrypoint,
-        address sender
+        address sender,
+        bytes memory _paymasterAndData
     ) internal returns (UserOperation[] memory ops) {
         uint256 nonce = entrypoint.getNonce(sender, 0);
 
@@ -31,13 +40,13 @@ contract TestPaymaster is BicTokenPaymasterTestBase {
             preVerificationGas: 500_000,
             maxFeePerGas: 500_000,
             maxPriorityFeePerGas: 500_000,
-            paymasterAndData: bytes(""),
+            paymasterAndData: _paymasterAndData,
             signature: bytes("")
         });
 
         // Sign UserOp
-        bytes32 opHash = EntryPoint(entrypoint).getUserOpHash(op);
-        bytes32 msgHash = ECDSA.toEthSignedMessageHash(opHash);
+        bytes32 opHash = entrypoint.getUserOpHash(op);
+        bytes32 msgHash = MessageHashUtils.toEthSignedMessageHash(opHash);
 
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(_signerPKey, msgHash);
         bytes memory userOpSignature = abi.encodePacked(r, s, v);
@@ -55,7 +64,8 @@ contract TestPaymaster is BicTokenPaymasterTestBase {
         address _target,
         uint256 _value,
         bytes memory _callData,
-        address sender
+        address sender,
+        bytes memory _paymasterAndData
     ) internal returns (UserOperation[] memory) {
         bytes memory callDataForEntrypoint = abi.encodeWithSignature(
             "execute(address,uint256,bytes)",
@@ -64,7 +74,7 @@ contract TestPaymaster is BicTokenPaymasterTestBase {
             _callData
         );
 
-        return _setupUserOp(_signerPKey, _initCode, callDataForEntrypoint, sender);
+        return _setupUserOp(_signerPKey, _initCode, callDataForEntrypoint, sender, _paymasterAndData);
     }
 
     function _setupUserOpExecuteBatch(
@@ -73,7 +83,8 @@ contract TestPaymaster is BicTokenPaymasterTestBase {
         address[] memory _target,
         uint256[] memory _value,
         bytes[] memory _callData,
-        address sender
+        address sender,
+        bytes memory _paymasterAndData
     ) internal returns (UserOperation[] memory) {
         bytes memory callDataForEntrypoint = abi.encodeWithSignature(
             "executeBatch(address[],uint256[],bytes[])",
@@ -82,6 +93,8 @@ contract TestPaymaster is BicTokenPaymasterTestBase {
             _callData
         );
 
-        return _setupUserOp(_signerPKey, _initCode, callDataForEntrypoint, sender);
+        return _setupUserOp(_signerPKey, _initCode, callDataForEntrypoint, sender, _paymasterAndData);
     }
+
+
 }
