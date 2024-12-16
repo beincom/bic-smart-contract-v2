2f5c797cadd043a2d4c0b22d1b5a789e5c8135d1
Vũ Phương Nam <vuphuongnam@evol.vn>
2024-12-16 10:54:36 +0700

    feat: BEIN-30520 [WIP] slot struct control

--- src/BicTokenPaymaster.sol
+++ src/BicTokenPaymaster.sol
@@ -18,8 +18,30 @@ import "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
  * @notice Using this paymaster mechanism for Account Abstraction bundler v0.6,
  * when need to change to bundler v0.7 or higher, using TokenPaymaster instead
  */
-contract BicTokenPaymaster is TokenSingletonPaymaster, PausableUpgradeable, UUPSUpgradeable {
-    bytes32 public constant IMPLEMENTATION_SLOT = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
+contract BicTokenPaymaster is
+    TokenSingletonPaymaster,
+    PausableUpgradeable,
+    UUPSUpgradeable
+{
+    /// @custom:storage-location erc7201:storage.BicTokenPaymaster
+    struct BicTokenPaymasterStorage {
+        /// The blocked users
+        mapping(address => bool) _isBlocked;
+    }
+
+    // keccak256(abi.encode(uint256(keccak256("storage.BicTokenPaymaster")) - 1)) & ~bytes32(uint256(0xff))
+    bytes32 private constant BicTokenPaymasterStorageLocation =
+        0x087f1ed82768b920bbf7f524ae10adce75c43e9e7db2301bbd943b1365e05e00;
+
+    function _getBicTokenPaymasterStorageLocation()
+        private
+        pure
+        returns (BicTokenPaymasterStorage storage $)
+    {
+        assembly {
+            $.slot := BicTokenPaymasterStorageLocation
+        }
+    }
 
     /// @dev Emitted when a user is blocked
     event BlockPlaced(address indexed _user, address indexed _operator);
@@ -32,7 +54,11 @@ contract BicTokenPaymaster is TokenSingletonPaymaster, PausableUpgradeable, UUPS
         _disableInitializers();
     }
 
-    function initialize(address _entryPoint, address _owner, address[] memory _singers) public initializer {
+    function initialize(
+        address _entryPoint,
+        address _owner,
+        address[] memory _singers
+    ) public initializer {
         __TokenSingletonPaymaster_init(_entryPoint, _singers);
         __ERC20_init("Beincom", "BIC");
         __ERC20Votes_init();
@@ -46,8 +72,10 @@ contract BicTokenPaymaster is TokenSingletonPaymaster, PausableUpgradeable, UUPS
      * @notice Blacklist a user.
      * @param _user the user to blacklist.
      */
-    function addToBlockedList (address _user) public onlyOwner {
-        isBlocked[_user] = true;
+    function addToBlockedList(address _user) public onlyOwner {
+        BicTokenPaymasterStorage
+            storage $ = _getBicTokenPaymasterStorageLocation();
+        $._isBlocked[_user] = true;
         emit BlockPlaced(_user, msg.sender);
     }
 
@@ -55,8 +83,10 @@ contract BicTokenPaymaster is TokenSingletonPaymaster, PausableUpgradeable, UUPS
      * @notice Unblock a user.
      * @param _user the user to unblock.
      */
-    function removeFromBlockedList (address _user) public onlyOwner {
-        isBlocked[_user] = false;
+    function removeFromBlockedList(address _user) public onlyOwner {
+        BicTokenPaymasterStorage
+            storage $ = _getBicTokenPaymasterStorageLocation();
+        $._isBlocked[_user] = false;
         emit BlockReleased(_user, msg.sender);
     }
 
@@ -80,25 +110,25 @@ contract BicTokenPaymaster is TokenSingletonPaymaster, PausableUpgradeable, UUPS
      * @dev Hook that is called before any transfer of tokens.
      * Override existing hook to add additional checks: paused and blocked users.
      */
-    function _update(address from, address to, uint256 amount) internal virtual override {
+    function _update(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual override {
+        BicTokenPaymasterStorage
+            storage $ = _getBicTokenPaymasterStorageLocation();
+
         super._update(from, to, amount);
 
         require(!paused(), "BicTokenPaymaster: token transfer while paused");
-        require(!isBlocked[from], "BicTokenPaymaster: sender is blocked");
-    }
-
-    /// @notice Returns the implementation of the ERC1967 proxy.
-    ///
-    /// @return $ The address of implementation contract.
-    function implementation() public view returns (address $) {
-        assembly {
-            $ := sload(IMPLEMENTATION_SLOT)
-        }
+        require(!$._isBlocked[from], "BicTokenPaymaster: sender is blocked");
     }
 
     /// @inheritdoc UUPSUpgradeable
     ///
     /// @dev Authorization logic is only based on the `msg.sender` being an owner of this account,
     ///      or `address(this)`.
-    function _authorizeUpgrade(address) internal view virtual override(UUPSUpgradeable) onlyOwner {}
+    function _authorizeUpgrade(
+        address
+    ) internal view virtual override(UUPSUpgradeable) onlyOwner {}
 }
