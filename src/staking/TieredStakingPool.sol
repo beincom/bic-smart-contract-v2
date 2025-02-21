27fae7c50fc6dd16748082e2c9453f80a60a7659
ntnam-bic <ngothanhnam@evol.vn>
2025-02-21 14:35:43 +0700

    (+) validation and decoration

--- src/staking/TieredStakingPool.sol
+++ src/staking/TieredStakingPool.sol
@@ -8,16 +8,13 @@ import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 contract TieredStakingPool is Ownable, ReentrancyGuard {
     using SafeERC20 for IERC20;
 
-    IERC20 public token;
-
+    // Structs
     struct Tier {
         uint256 maxTokens;
         uint256 annualInterestRate;
         uint256 lockDuration;
         uint256 totalStaked;
     }
-
-    Tier[] public tiers;
 
     struct Deposit {
         uint256 amount;
@@ -26,29 +23,68 @@ contract TieredStakingPool is Ownable, ReentrancyGuard {
         bool withdrawn;
     }
 
+    IERC20 public token;
+    Tier[] public tiers;
     mapping(address => Deposit[]) public deposits;
 
+    // Events
+
+    /// @notice Emitted when adding a new tiered staking pool
     event TierAdded(uint256 indexed tierIndex, uint256 maxTokens, uint256 annualInterestRate, uint256 lockDuration);
+    /// @notice Emitted when depositing staking in the tiered staking pool
     event Deposited(address indexed user, uint256 amount, uint256 tierIndex);
+    /// @notice Emitted when unlocking staking in the tiered staking pool after the maturity
     event Withdrawn(address indexed user, uint256 principal, uint256 interest);
 
-    error ZeroTokenAddress();
+    // Errors
+    error ZeroAddress();
+    error InvalidInterestRate();
+    error InvalidLockDuration();
     error ZeroStakeAmount();
     error NotEnoughCapacityInTier();
+    error InvalidStartIndex();
     error ZeroWithdrawAmount();
 
     constructor(IERC20 _token, address _owner) Ownable(_owner) {
-        if (address(_token) == address(0)) {
-            revert ZeroTokenAddress();
+        if (address(_token) == address(0) || _owner == address(0)) {
+            revert ZeroAddress();
         }
         token = _token;
     }
 
+    /**
+     * @notice Getting all tiered staking pools
+     */
+    function getTiers() external view returns (Tier[] memory) {
+        return tiers;
+    }
+
+    /**
+     * @notice Getting all users' deposits
+     * @param user The user address
+     */
+    function getUserDeposits(address user) external view returns (Deposit[] memory) {
+        return deposits[user];
+    }
+
+    /**
+     * @notice Added a tiered staking pool
+     * @param _maxTokens The maximum tokens can be staked in the tiered staking pool
+     * @param _annualInterestRate The percentage of annual interest rate
+     * @param _lockDuration The period time of staking
+     */
     function addTier(
         uint256 _maxTokens,
         uint256 _annualInterestRate,
         uint256 _lockDuration
     ) external onlyOwner {
+        if (_annualInterestRate > 10000) {
+            revert InvalidInterestRate();
+        }
+        if (_lockDuration == 0) {
+            revert InvalidLockDuration();
+        }
+
         tiers.push(
             Tier({
                 maxTokens: _maxTokens,
@@ -60,6 +96,10 @@ contract TieredStakingPool is Ownable, ReentrancyGuard {
         emit TierAdded(tiers.length - 1, _maxTokens, _annualInterestRate, _lockDuration);
     }
 
+    /**
+     * @notice Depositing tokens in the tiered staking pools
+     * @param amount The amount of tokens staked in the tiered staking pools
+     */
     function deposit(uint256 amount) external nonReentrant {
         if (amount == 0) {
             revert ZeroStakeAmount();
@@ -90,8 +130,17 @@ contract TieredStakingPool is Ownable, ReentrancyGuard {
         }
     }
 
+    /**
+     * @notice Withdrawing staking tokens after the maturity
+     * @param startIndex The start index to look up in the users' deposits
+     * @param batchSize The size will be loop from the start index in the users' deposits
+     */
     function withdrawBatch(uint256 startIndex, uint256 batchSize) external nonReentrant {
         Deposit[] storage userDeposits = deposits[msg.sender];
+
+        if (startIndex >= userDeposits.length) {
+            revert InvalidStartIndex();
+        }
         uint256 totalPrincipal;
         uint256 totalInterest;
         uint256 processed = 0;
@@ -129,6 +178,9 @@ contract TieredStakingPool is Ownable, ReentrancyGuard {
         emit Withdrawn(msg.sender, totalPrincipal, totalInterest);
     }
 
+    /**
+     * @notice Withdrawing all staking tokens after the maturity
+     */
     function withdrawAll() external nonReentrant {
         Deposit[] storage userDeposits = deposits[msg.sender];
         uint256 totalPrincipal;
@@ -157,13 +209,5 @@ contract TieredStakingPool is Ownable, ReentrancyGuard {
         uint256 payout = totalPrincipal + totalInterest;
         token.safeTransfer(msg.sender, payout);
         emit Withdrawn(msg.sender, totalPrincipal, totalInterest);
-    }
-
-    function getTiers() external view returns (Tier[] memory) {
-        return tiers;
-    }
-
-    function getUserDeposits(address user) external view returns (Deposit[] memory) {
-        return deposits[user];
     }
 }
