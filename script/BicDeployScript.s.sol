9d1dc308100cc67e6102d2453cca8d96a423d620
ntnam-bic <ngothanhnam@evol.vn>
2025-04-10 09:21:34 +0700

    chore: deploy script for BIC token

--- script/BicDeployScript.s.sol
+++ script/BicDeployScript.s.sol
@@ -6,14 +6,14 @@ import {console} from "forge-std/console.sol";
 import {BicTokenPaymaster} from "../src/BicTokenPaymaster.sol";
 
 contract BicDeployScript is Script {
-    address entrypoint = 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789;
-    address superController = 0xaCc34513D10bd5c57109bF83A8754856FF0b0eb0;
-    address[] signers = [0xaCc34513D10bd5c57109bF83A8754856FF0b0eb0];
-
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        address entrypoint = vm.envAddress("SMART_ACCOUNT_ENTRY_POINT");
+        address superController = vm.envAddress("BIC_SUPPER_CONTROLLER");
+        address[] memory offchainVerifiers = new address[](1);
+        offchainVerifiers[0] = vm.envAddress("BIC_OFF_CHAIN_VERIFIER");
         vm.startBroadcast(deployerPrivateKey);
-        BicTokenPaymaster bic = new BicTokenPaymaster(entrypoint, superController, signers);
+        BicTokenPaymaster bic = new BicTokenPaymaster(entrypoint, superController, offchainVerifiers);
         console.log("Bic Token Paymaster deployed contract:", address (bic));
 
         vm.stopBroadcast();
