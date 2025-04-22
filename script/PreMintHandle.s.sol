be57029e59f7b03510d49caa81a5dfaa19c020b1
0xted97 <vietkhiem@evol.vn>
2025-04-22 11:46:02 +0700

    feat: script pre mint nft

--- script/PreMintHandle.s.sol
+++ script/PreMintHandle.s.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.23;
 import {Script} from "forge-std/Script.sol";
 import {console} from "forge-std/console.sol";
 import {HandlesController} from "../src/namespaces/HandlesController.sol";
+import {Handles} from "../src/namespaces/Handles.sol";
 import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
@@ -53,7 +54,7 @@ contract PremintNFTScript is Script {
     address public coNFTHandle;
     address public receiver;
     address public multicall;
-    uint256 public signerPrivateKey;
+    uint256 public operatorPrivateKey;
 
     // Struct to hold handle request data
     struct HandleRequestData {
@@ -63,18 +64,19 @@ contract PremintNFTScript is Script {
 
     function setUp() public {
         // Update addresses
-        handlesController = 0x12B080bc45c4e26D5cb71F8b054893c0d245f111;
-        uoNFTHandle = 0x08e39687442D122F915b8d944896A8dcd33dDC62;
-        poNFTHandle = 0x2959E2CE1980602f9c9D0CFB12B160Dfa36Dba4F;
-        coNFTHandle = 0xbE99dd369BE5c484752970478F27d53d882db072;
-        receiver = 0x379C1632753B1D0628071C1d138D6C513a2fDB8b;
+        handlesController = vm.envAddress("CONTROLLER_ADDRESS");
+        uoNFTHandle = vm.envAddress("NFT_OWNERSHIP_USERNAME_COLLECTION_ADDRESS");
+        poNFTHandle = vm.envAddress("NFT_OWNERSHIP_PERSONAL_NAME_COLLECTION_ADDRESS");
+        coNFTHandle = vm.envAddress("NFT_OWNERSHIP_COMMUNITY_NAME_COLLECTION_ADDRESS");
+        receiver = vm.envAddress("PREMINT_RECEIVER_ADDRESS");
         multicall = 0xcA11bde05977b3631167028862bE2a173976CA11;
-        signerPrivateKey = vm.envUint("NFT_VERIFIER_PRIVATE_KEY");
+        operatorPrivateKey = vm.envUint("NFT_OPERATOR_PRIVATE_KEY");
     }
 
     function run() public {
 
         HandleRequestData[] memory allHandles = getAllHandleRequests();
+        // HandleRequestData[] memory allHandles = getAllHandleRequestsForTest();
         IMulticall3.Call3[] memory calls = new IMulticall3.Call3[](allHandles.length);
         
         // Empty arrays for beneficiaries and collects
@@ -108,7 +110,7 @@ contract PremintNFTScript is Script {
                 validAfter
             );
             bytes32 ethSignedMessageHash = MessageHashUtils.toEthSignedMessageHash(messageHash);
-            (uint8 v, bytes32 r, bytes32 s) = vm.sign(signerPrivateKey, ethSignedMessageHash);
+            (uint8 v, bytes32 r, bytes32 s) = vm.sign(operatorPrivateKey, ethSignedMessageHash);
             bytes memory signature = abi.encodePacked(r, s, v);
 
             // Prepare multicall data
@@ -132,40 +134,83 @@ contract PremintNFTScript is Script {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
         IMulticall3(multicall).aggregate3(calls);
+
+
+        // Verify all handles are owned by receiver
+        for (uint256 i = 0; i < allHandles.length; i++) {
+            Handles handle = Handles(allHandles[i].handleContract);
+            require(
+                handle.ownerOf(handle.getTokenId(allHandles[i].name)) == receiver,
+                "Handle NFT not owned by receiver"
+            );
+        }
+        
         vm.stopBroadcast();
     }
 
     function getAllHandleRequests() internal view returns (HandleRequestData[] memory) {
         // Username Ownership NFTs (uoNFT)
         HandleRequestData[] memory uoHandles = new HandleRequestData[](9);
-        uoHandles[0] = HandleRequestData("contact", uoNFTHandle);
-        uoHandles[1] = HandleRequestData("bicsupport", uoNFTHandle);
-        uoHandles[2] = HandleRequestData("beincom.global.admin", uoNFTHandle);
-        uoHandles[3] = HandleRequestData("crisbicsupport", uoNFTHandle);
-        uoHandles[4] = HandleRequestData("lilybicsupport", uoNFTHandle);
-        uoHandles[5] = HandleRequestData("canarybicsupport", uoNFTHandle);
-        uoHandles[6] = HandleRequestData("dylanbicsupport", uoNFTHandle);
-        uoHandles[7] = HandleRequestData("beincom", uoNFTHandle);
-        uoHandles[8] = HandleRequestData("beincomadmin", uoNFTHandle);
+        uoHandles[0] = HandleRequestData(unicode"contact", uoNFTHandle);
+        uoHandles[1] = HandleRequestData(unicode"bicsupport", uoNFTHandle);
+        uoHandles[2] = HandleRequestData(unicode"beincom.global.admin", uoNFTHandle);
+        uoHandles[3] = HandleRequestData(unicode"crisbicsupport", uoNFTHandle);
+        uoHandles[4] = HandleRequestData(unicode"lilybicsupport", uoNFTHandle);
+        uoHandles[5] = HandleRequestData(unicode"canarybicsupport", uoNFTHandle);
+        uoHandles[6] = HandleRequestData(unicode"dylanbicsupport", uoNFTHandle);
+        uoHandles[7] = HandleRequestData(unicode"beincom", uoNFTHandle);
+        uoHandles[8] = HandleRequestData(unicode"beincomadmin", uoNFTHandle);
 
         // Personal Name Ownership NFTs (poNFT)
         HandleRequestData[] memory poHandles = new HandleRequestData[](9);
-        poHandles[0] = HandleRequestData("Beincom Official", poNFTHandle);
-        poHandles[1] = HandleRequestData("Beincom Vietnam Admin", poNFTHandle);
-        poHandles[2] = HandleRequestData("Beincom Global Admin", poNFTHandle);
-        poHandles[3] = HandleRequestData("Lily Beincom Support", poNFTHandle);
-        poHandles[4] = HandleRequestData("Canary Beincom Support", poNFTHandle);
-        poHandles[5] = HandleRequestData("Cris Beincom Support", poNFTHandle);
-        poHandles[6] = HandleRequestData("Dylan Beincom Support", poNFTHandle);
-        poHandles[7] = HandleRequestData("Beincom", poNFTHandle);
-        poHandles[8] = HandleRequestData("Beincom Admin", poNFTHandle);
+        poHandles[0] = HandleRequestData(unicode"Beincom Official", poNFTHandle);
+        poHandles[1] = HandleRequestData(unicode"Beincom Vietnam Admin", poNFTHandle);
+        poHandles[2] = HandleRequestData(unicode"Beincom Global Admin", poNFTHandle);
+        poHandles[3] = HandleRequestData(unicode"Lily Beincom Support", poNFTHandle);
+        poHandles[4] = HandleRequestData(unicode"Canary Beincom Support", poNFTHandle);
+        poHandles[5] = HandleRequestData(unicode"Cris Beincom Support", poNFTHandle);
+        poHandles[6] = HandleRequestData(unicode"Dylan Beincom Support", poNFTHandle);
+        poHandles[7] = HandleRequestData(unicode"Beincom", poNFTHandle);
+        poHandles[8] = HandleRequestData(unicode"Beincom Admin", poNFTHandle);
 
         // Community Name Ownership NFTs (coNFT)
         HandleRequestData[] memory coHandles = new HandleRequestData[](4);
-        coHandles[0] = HandleRequestData("Beincom Viet Nam", coNFTHandle);
-        coHandles[1] = HandleRequestData("Beincom Global", coNFTHandle);
-        coHandles[2] = HandleRequestData("Beincom's EchoSphere", coNFTHandle);
-        coHandles[3] = HandleRequestData("Beincom", coNFTHandle);
+        coHandles[0] = HandleRequestData(unicode"Beincom Việt Nam", coNFTHandle);
+        coHandles[1] = HandleRequestData(unicode"Beincom Global", coNFTHandle);
+        coHandles[2] = HandleRequestData(unicode"Beincom's EchoSphere", coNFTHandle);
+        coHandles[3] = HandleRequestData(unicode"Beincom", coNFTHandle);
+
+        // Combine all handles
+        HandleRequestData[] memory allHandles = new HandleRequestData[](
+            uoHandles.length + poHandles.length + coHandles.length
+        );
+        
+        uint256 currentIndex = 0;
+        for (uint256 i = 0; i < uoHandles.length; i++) {
+            allHandles[currentIndex++] = uoHandles[i];
+        }
+        for (uint256 i = 0; i < poHandles.length; i++) {
+            allHandles[currentIndex++] = poHandles[i];
+        }
+        for (uint256 i = 0; i < coHandles.length; i++) {
+            allHandles[currentIndex++] = coHandles[i];
+        }
+
+        return allHandles;
+    }
+
+    function getAllHandleRequestsForTest() internal view returns (HandleRequestData[] memory) {
+        // Username Ownership NFTs (uoNFT)
+        HandleRequestData[] memory uoHandles = new HandleRequestData[](1);
+        uoHandles[0] = HandleRequestData(unicode"testusername", uoNFTHandle);
+
+        // Personal Name Ownership NFTs (poNFT)
+        HandleRequestData[] memory poHandles = new HandleRequestData[](1);
+        poHandles[0] = HandleRequestData(unicode"Test tên cá nhân", poNFTHandle);
+
+        // Community Name Ownership NFTs (coNFT)
+        HandleRequestData[] memory coHandles = new HandleRequestData[](1);
+        coHandles[0] = HandleRequestData(unicode"Test tên cộng đồng", coNFTHandle);
 
         // Combine all handles
         HandleRequestData[] memory allHandles = new HandleRequestData[](
