cb55d78b0071f00047d0adb3a1802db7c1bf68e9
ntnam-bic <ngothanhnam@evol.vn>
2025-06-12 15:53:33 +0700

    (+) script deploy and setup for duplicated handles and controller

---
+++ script/nft/DeployAndSetupDupHandlesController.s.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.23;
+
+import {Script} from "forge-std/Script.sol";
+import {console} from "forge-std/console.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {Handles} from "../../src/namespaces/Handles.sol";
+import {DupHandlesController} from "../../src/namespaces/DupHandlesController.sol";
+import {BicForwarder} from "../../src/forwarder/BicForwarder.sol";
+
+contract DeployAndSetupDupHandlesController is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        address marketplaceAddress = vm.envAddress("MARKETPLACE_ADDRESS"); 
+        address treasury = vm.envAddress("TREASURY_ADDRESS");
+        address forwarder = vm.envAddress("FORWARDER_ADDRESS");
+        address operator = vm.envAddress("HANDLE_CONTROLLER_OPERATOR_ADDRESS");
+        address bic = vm.envAddress("BIC_ADDRESS");
+        address dupHandlesControllerOwner = vm.envAddress("DUPHANDLE_CONTROLLER_OWNER"); 
+        DupHandlesController dupHandlesController = new DupHandlesController(IERC20(bic), dupHandlesControllerOwner);
+       
+        console.log("HandleController deployed at:", address(dupHandlesController));
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        dupHandlesController.setMarketplace(marketplaceAddress);
+        dupHandlesController.setForwarder(forwarder);
+        dupHandlesController.setCollector(treasury);
+
+        dupHandlesController.setOperator(operator);
+
+        BicForwarder bicForwarder = BicForwarder(forwarder);
+        bicForwarder.addController(address(dupHandlesController));
+        
+        vm.stopBroadcast();
+    }
+}
+
--- script/nft/DupHandlesDeployScript.s.sol
+++ script/nft/DupHandlesDeployScript.s.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.8.23;
 
 import {Script} from "forge-std/Script.sol";
 import {console} from "forge-std/console.sol";
-import {DupHandles} from "../src/namespaces/DupHandles.sol";
-import {HandleTokenURI} from "../src/namespaces/HandleTokenURI.sol";
-contract HandleNftDupDeployScript is Script {
+import {DupHandles} from "../../src/namespaces/DupHandles.sol";
+import {HandleTokenURI} from "../../src/namespaces/HandleTokenURI.sol";
+contract DupHandlesDeployScript is Script {
     struct NFTData {
         string namespace;
         string name;
@@ -26,13 +26,13 @@ contract HandleNftDupDeployScript is Script {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         address deployOwner = vm.addr(deployerPrivateKey);
         address handleTokenURIAddress = vm.envAddress("HANDLE_TOKEN_URI_ADDRESS");
-        address controllerAddress = vm.envAddress("CONTROLLER_ADDRESS");
+        address dupHandlesController = vm.envAddress("DUPHANDLE_CONTROLLER_ADDRESS");
         vm.startBroadcast(deployerPrivateKey);
 
         DupHandles dupHandles = new DupHandles();
         dupHandles.initialize(nftData.namespace, nftData.name, nftData.symbol, deployOwner);
         dupHandles.setHandleTokenURIContract(handleTokenURIAddress);
-        dupHandles.setController(controllerAddress);
+        dupHandles.setController(dupHandlesController);
 
         HandleTokenURI handleTokenURI = HandleTokenURI(handleTokenURIAddress);
         handleTokenURI.setNameElement(nftData.namespace, nftData.imageDescription, nftData.imageUri);
